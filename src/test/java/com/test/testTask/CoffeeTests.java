a98a774b7203ce8d0cdea65f7316d851de380a01
Alex <mcs666554@gmail.com>
2024-02-14 15:03:33 +0300

    2 passed test

--- src/test/java/com/test/testTask/CoffeeTests.java
+++ src/test/java/com/test/testTask/CoffeeTests.java
@@ -9,18 +9,18 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest
 @AutoConfigureMockMvc
-class CoffeeIntegrationTests {
+class CoffeeTests {
     @Autowired
     private MockMvc mockMvc;
 
@@ -28,6 +28,7 @@ class CoffeeIntegrationTests {
     private ObjectMapper objectMapper;
 
     @Test
+    @Transactional
     void testCreateCup() throws Exception {
         Cup cup = new Cup();
         cup.setValue(0.4f);
@@ -38,16 +39,28 @@ class CoffeeIntegrationTests {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.value").value(cup.getValue())); // проверяем ожидаемое значение
+                .andExpect(jsonPath("$.balance").value(cup.getBalance()))
+                .andExpect(jsonPath("$.value").value(cup.getValue()));
     }
 
+    @Transactional
     @Test
-    void testAnyCoffeeMakeWithAddedResources() {
+    void testCreateGrade() throws Exception {
+        Grade grade = new Grade();
+        grade.setName("Gentleman");
+        grade.setBalance(4);
+        grade.setRoast(10);
 
-//        String gradeUrl = "http://localhost:8080/api/staff/grade/add";
-//        Grade grade = new Grade("robusto", 8, 2);
-//        ResponseEntity<Grade> gradeResponse = restTemplate.postForObject(gradeUrl, grade, ResponseEntity.class);
-//        assertEquals(ResponeS, gradeResponse.getStatusCode());
+        mockMvc.perform(post("/api/staff/grade/add")
+                .content(objectMapper.writeValueAsString(grade))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value(grade.getName()))
+                .andExpect(jsonPath("$.balance").value(grade.getBalance()))
+                .andExpect(jsonPath("$.roast").value(grade.getRoast()));
     }
+
+
 }
 
